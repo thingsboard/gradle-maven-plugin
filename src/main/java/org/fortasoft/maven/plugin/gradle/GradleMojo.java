39dff52fad8c64631bfc866d9f68bfc46a7f31d6
Viacheslav Klimov <viacheslavklimov11@gmail.com>
2026-02-05 11:20:32 +0200

    Migrate to Java 25

--- src/main/java/org/fortasoft/maven/plugin/gradle/GradleMojo.java
+++ src/main/java/org/fortasoft/maven/plugin/gradle/GradleMojo.java
@@ -57,7 +57,7 @@ public class GradleMojo extends AbstractMojo {
 	*/
 	public static final String SYS_PREFIX = "gradle.sys.";
 
-	@Parameter(defaultValue="8.6", required=true)
+	@Parameter(defaultValue="8.14", required=true)
 	private String gradleVersion;
 
 
